ada693e75b4aa113bc56f37b87774e8fa1d614d1
Devansh Bhagania <devanshbhagania19@gmail.com>
2024-09-15 12:55:28 +0530

    Add code for slices in Go

---
+++ basics/14_slice.go
@@ -0,0 +1,35 @@
+/*
+Slices are similar to arrays, but are more powerful and flexible.
+
+Like arrays, slices are also used to store multiple values of the same type in a single variable.
+
+However, unlike arrays, the length of a slice can grow and shrink as you see fit.
+
+In Go, there are several ways to create a slice:
+
+Using the []datatype{values} format
+Create a slice from an array
+Using the make() function
+*/
+
+// slice_name := []datatype{values}
+// myslice := []int{}
+// myslice := []int{1,2,3}
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	myslice1 := []int{}
+	fmt.Println(len(myslice1))
+	fmt.Println(cap(myslice1))
+	fmt.Println(myslice1)
+
+	myslice2 := []string{"Go", "Slices", "Are", "Powerful"}
+	fmt.Println(len(myslice2))
+	fmt.Println(cap(myslice2))
+	fmt.Println(myslice2)
+}
