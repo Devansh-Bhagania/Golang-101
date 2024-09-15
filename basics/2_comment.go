7076aa08356fdf94efe32018328408d68bb17187
Devansh Bhagania <devanshbhagania19@gmail.com>
2024-09-15 12:53:40 +0530

    Add comments to Hello World program

---
+++ basics/2_comment.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	/* The code below will print Hello World
+	to the screen, and it is amazing */
+
+	// this is single line comment
+	fmt.Println("Hello World!")
+}
